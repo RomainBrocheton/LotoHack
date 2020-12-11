bf30c5d66cca190df1b0fb0fb49a00579bec698b
Romain Brocheton <romain.brocheton@etu.univ-amu.fr>
2020-12-11 15:25:54 +0100

    Début d'interface d'importation

--- view/importationView.php
+++ view/importationView.php
@@ -6,6 +6,16 @@
 
 <h1>Importation</h1>
 
+<p>Cette page vous permet d'importer de nouveaux tirages à la base de données de LotoHACK. Vous pouvez choisir d'importer manuellement un fichier ou d'utiliser l'importation automatique.</p>
+
+<div id="importation">
+    <button class="btn-gradient-hover">
+        <i class="fas fa-bolt"></i>
+        <span>Importation automatique</span>
+    </button>
+    <p>ou <a href="importation.php?manuelle">importer manuellement un fichier</a></p>
+</div>
+
 <?php 
     $content = ob_get_clean();
 
