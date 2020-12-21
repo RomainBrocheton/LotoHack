0a388e42ff98869a853d31b8d27d0e92371f2e24
Romain Brocheton <romain.brocheton@etu.univ-amu.fr>
2020-12-21 21:49:24 +0100

    Merge with BDD - Migration des tables

--- model/frontend.php
+++ model/frontend.php
@@ -2,7 +2,7 @@
     /***
      * Models : only data binding. It should not contain any presentation logic.
      */
-    function db_connect() {
+    function db_connect(){
         try {
             $db = new PDO('mysql:host=localhost;dbname=genielogiciel;charset=utf8', 'root', 'root') ;
             return $db ;
